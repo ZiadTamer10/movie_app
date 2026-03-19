10bc19d84d22b33f4b24f59590068bdbc81568e3
Ziad Tamer <ztamer494@gmail.com>
2026-03-19 13:55:36 +0200

    add styles to our app

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
