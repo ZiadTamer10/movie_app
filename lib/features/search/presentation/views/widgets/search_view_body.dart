1dc6debe92e2490f4ece3e6974eb483449012382
Ziad Tamer <ztamer494@gmail.com>
2026-03-24 18:15:34 +0200

    create appbar for for search view done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/core/widgets/custom_core_app_bar.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(children: [CustomCoreAppBar()]);
   }
 }
