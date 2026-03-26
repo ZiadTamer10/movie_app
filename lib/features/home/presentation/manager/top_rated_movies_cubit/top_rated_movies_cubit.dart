e88dc072e36a66ffa648205c2349accc8c6f2150
Ziad Tamer <ztamer494@gmail.com>
2026-03-26 19:40:30 +0200

    create top rated movies cubit

--- lib/features/home/presentation/manager/top_rated_movies_cubit/top_rated_movies_cubit.dart
+++ lib/features/home/presentation/manager/top_rated_movies_cubit/top_rated_movies_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_app/features/home/data/model/movie_model.dart';
+import 'package:movie_app/features/home/data/repos/home_repo.dart';
 
 part 'top_rated_movies_state.dart';
 
 class TopRatedMoviesCubit extends Cubit<TopRatedMoviesState> {
-  TopRatedMoviesCubit() : super(TopRatedMoviesInitial());
+  TopRatedMoviesCubit(this.homeRepo) : super(TopRatedMoviesInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchTopRatedMovies() async {
+    emit(TopRatedMoviesLoading());
+    var result = await homeRepo.fetchTopRatedMovies();
+    result.fold(
+      (failure) {
+        emit(TopRatedMoviesFailure(failure.errMessage));
+      },
+      (movies) {
+        emit(TopRatedMoviesSuccess(movies));
+      },
+    );
+  }
 }
