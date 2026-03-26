559a461a15132897c86896d49b9b7d7adc1e6840
Ziad Tamer <ztamer494@gmail.com>
2026-03-26 16:51:58 +0200

    create home repo methods and failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/core/errors/failure.dart';
+import 'package:movie_app/features/home/data/model/movie_model.dart';
+
 abstract class HomeRepo {
-  fetchPopularMovies();
-  fetchTopRatedMovies();
-  fetchNowPlayingMovies();
+  Future<Either<Failure, List<MovieModel>>> fetchPopularMovies();
+  Future<Either<Failure, List<MovieModel>>> fetchTopRatedMovies();
+  Future<Either<Failure, List<MovieModel>>> fetchNowPlayingMovies();
 }
