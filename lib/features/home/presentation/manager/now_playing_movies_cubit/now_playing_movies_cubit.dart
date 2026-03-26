8fbe80012361c82c96b149a1d559abaca9e46861
Ziad Tamer <ztamer494@gmail.com>
2026-03-26 19:46:40 +0200

    create now playing movies cubit

--- lib/features/home/presentation/manager/now_playing_movies_cubit/now_playing_movies_cubit.dart
+++ lib/features/home/presentation/manager/now_playing_movies_cubit/now_playing_movies_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_app/features/home/data/model/movie_model.dart';
+import 'package:movie_app/features/home/data/repos/home_repo.dart';
 
 part 'now_playing_movies_state.dart';
 
 class NowPlayingMoviesCubit extends Cubit<NowPlayingMoviesState> {
-  NowPlayingMoviesCubit() : super(NowPlayingMoviesInitial());
+  NowPlayingMoviesCubit(this.homeRepo) : super(NowPlayingMoviesInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNowPlayingMovies() async {
+    emit(NowPlayingMoviesLoading());
+
+    var result = await homeRepo.fetchNowPlayingMovies();
+    result.fold(
+      (failure) => emit(NowPlayingMoviesFailure(failure.errMessage)),
+      (movies) => emit(NowPlayingMoviesSuccess(movies)),
+    );
+  }
 }
