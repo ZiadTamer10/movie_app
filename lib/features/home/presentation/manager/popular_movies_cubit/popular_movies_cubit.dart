bd9982b1e704c6f01c12cd0090bbcc5e4382200e
Ziad Tamer <ztamer494@gmail.com>
2026-03-26 19:34:57 +0200

    create popular movies cubit methods

--- lib/features/home/presentation/manager/popular_movies_cubit/popular_movies_cubit.dart
+++ lib/features/home/presentation/manager/popular_movies_cubit/popular_movies_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_app/features/home/data/model/movie_model.dart';
+import 'package:movie_app/features/home/data/repos/home_repo.dart';
 
 part 'popular_movies_state.dart';
 
 class PopularMoviesCubit extends Cubit<PopularMoviesState> {
-  PopularMoviesCubit() : super(PopularMoviesInitial());
+  PopularMoviesCubit(this.homeRepo) : super(PopularMoviesInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchPopularMovies() async {
+    emit(PopularMoviesLoading());
+    var result = await homeRepo.fetchPopularMovies();
+    result.fold(
+      (failure) => emit(PopularMoviesFailure(failure.errMessage)),
+      (movies) => emit(PopularMoviesSuccess(movies)),
+    );
+  }
 }
