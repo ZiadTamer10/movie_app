4719eb621e69728c8fda57831e6cd191b25ff95f
Ziad Tamer <ztamer494@gmail.com>
2026-03-28 17:29:51 +0200

    create popular tv cubit

--- lib/features/home/presentation/manager/popular_tv_cubit/popular_tv_cubit.dart
+++ lib/features/home/presentation/manager/popular_tv_cubit/popular_tv_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_app/features/home/data/model/movie_model.dart';
+import 'package:movie_app/features/home/data/repos/home_repo.dart';
 
 part 'popular_tv_state.dart';
 
 class PopularTvCubit extends Cubit<PopularTvState> {
-  PopularTvCubit() : super(PopularTvInitial());
+  PopularTvCubit(this.homeRepo) : super(PopularTvInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchPopularTV() async {
+    emit(PopularTvLoading());
+    var result = await homeRepo.fetchPopulatTV();
+
+    result.fold(
+      (failure) => emit(PopularTvFailure(failure.errMessage)),
+      (tv) => emit(PopularTvSuccess(tv)),
+    );
+  }
 }
