c6fda51bb82645b4be89965a4bc6da9771276644
Ziad Tamer <ztamer494@gmail.com>
2026-03-31 17:48:26 +0200

    create similar cubit

--- lib/features/home/presentation/manager/similar_cubit/similar_cubit.dart
+++ lib/features/home/presentation/manager/similar_cubit/similar_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_app/features/home/data/model/movie_model.dart';
+import 'package:movie_app/features/home/data/repos/home_repo.dart';
 
 part 'similar_state.dart';
 
 class SimilarCubit extends Cubit<SimilarState> {
-  SimilarCubit() : super(SimilarInitial());
+  SimilarCubit(this.homeRepo) : super(SimilarInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilar() async {
+    emit(SimilarLoading());
+    var result = await homeRepo.fetchSimilar(id: 500);
+    result.fold(
+      (failure) => SimilarFailure(failure.errMessage),
+      (similar) => SimilarSuccess(similar),
+    );
+  }
 }
