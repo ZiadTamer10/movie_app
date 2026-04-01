e801b38d7dd741ae3684e4aba3d0482e7148656e
Ziad Tamer <ztamer494@gmail.com>
2026-04-01 21:15:06 +0200

    create serach cubit done

--- lib/features/search/presentation/manager/search_cubit/search_cubit.dart
+++ lib/features/search/presentation/manager/search_cubit/search_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_app/core/model/movie_model.dart';
+import 'package:movie_app/features/search/data/repos/search_repo.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+
+  final SearchRepo searchRepo;
+
+  Future<void> fetchSearch({
+    required String type,
+    required String query,
+  }) async {
+    emit(SearchLoading());
+
+    var result = await searchRepo.fetchSearch(type: type, query: query);
+
+    result.fold(
+      (failure) => emit(SearchFailure(failure.errMessage)),
+      (search) => emit(SearchSuccess(search)),
+    );
+  }
 }
