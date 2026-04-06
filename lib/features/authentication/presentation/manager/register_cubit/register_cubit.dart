d71d2786d5d3ed252eba70339462affb4349ecff
Ziad Tamer <ztamer494@gmail.com>
2026-04-07 00:02:27 +0200

    create register cubit done

--- lib/features/authentication/presentation/manager/register_cubit/register_cubit.dart
+++ lib/features/authentication/presentation/manager/register_cubit/register_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:movie_app/features/authentication/data/repos/auth_repo.dart';
 
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
-  RegisterCubit() : super(RegisterInitial());
+  RegisterCubit(this.authRepo) : super(RegisterInitial());
+
+  final AuthRepo authRepo;
+
+  Future<void> register({
+    required String email,
+    required String password,
+  }) async {
+    emit(RegisterLoading());
+    var result = await authRepo.register(email: email, password: password);
+    result.fold(
+      (failure) => emit(RegisterFailure(failure.errMessage)),
+      (user) => emit(RegisterSuccess(user)),
+    );
+  }
 }
