8df1605f5e0b4993361fcc159562f03985cf0e47
Ziad Tamer <ztamer494@gmail.com>
2026-04-06 19:41:29 +0200

    create custom auth button

---
+++ lib/features/authentication/presentation/views/widgets/custom_auth_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/utils/styles.dart';
+
+class CustomAuthButton extends StatelessWidget {
+  const CustomAuthButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SizedBox(
+        height: 55,
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color.fromARGB(255, 234, 23, 8),
+          ),
+          child: Text(
+            'Sign In',
+            style: Styles.textStyle16.copyWith(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/authentication/presentation/views/widgets/login_view_body.dart
+++ lib/features/authentication/presentation/views/widgets/login_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/utils/styles.dart';
+import 'package:movie_app/features/authentication/presentation/views/widgets/custom_auth_button.dart';
 import 'package:movie_app/features/authentication/presentation/views/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -17,6 +18,8 @@ class LoginViewBody extends StatelessWidget {
         SizedBox(height: 15),
         CustomTextFormField('Email'),
         CustomTextFormField('Password'),
+        SizedBox(height: 50),
+        CustomAuthButton(),
       ],
     );
   }
